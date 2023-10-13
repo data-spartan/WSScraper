82bb09d9ec370856d0623bf01af4f1e93e2e6add
data-spartan <data.spartaan@gmail.com>
2023-10-13 15:31:08 +0200

    [IB-2] IMplements error handlign

--- utils/ws_interface.py
+++ utils/ws_interface.py
@@ -1,4 +1,5 @@
 import threading
+import sys
 import websocket
 from utils import redis_hash
 from utils.parser_tools import *
@@ -10,12 +11,6 @@ from time import sleep
 # from constants import random_ids_list,scrapoxy_admin_pass_base64
 from utils.log_func import *
 
-load_dotenv(".env")
-"""
-returning filehandler and logger instance for websocket logging purposes
-"""
-fh, logg = logging_func("websocket",getenv("main_logs"))
-
 @dataclass
 class WebsocketClient:
     ws_url: AnyStr = field(default=str,repr=False)
@@ -24,13 +19,17 @@ class WebsocketClient:
     result_redis: List[Dict]= field(default_factory=dict, repr=False)
     random_ids_list: List= field(default_factory=lambda: pd.read_csv(getenv("ids_random_path"),header=None)[0].to_list(),repr=False)
     parsers: Any = field(default_factory=Parsers,repr=False)
-    """
-    Exclude some attributes from __str__ representation of a dataclass
-    """
+    
+    def __post_init__(self):
+        self.fh, self.logg = logging_func("ws", getenv("main_logs"))
+        self.pill2kill = threading.Event() #---ensures if error happens that potential hanging thread is closed
+        self.conn_closed_count=0
+        self.error_count=0
+    
 
     def subsription(self,ws: websocket.WebSocketApp) -> None:
-        logg.info(f"{threading.current_thread().name} thread started. Thread id: {threading.get_ident()}")
-        while True:
+        self.logg.info(f"{threading.current_thread().name} thread started. Thread id: {threading.get_ident()}")
+        while not self.pill2kill.is_set():
             """
             first request is for matches data, second for related markets;
             need to add sleep(2) bcs sometimes happens that there are no messages to read from, so socket closes connection and error fires up.
@@ -40,10 +39,11 @@ class WebsocketClient:
                 '{"command":"get","params":{"source":"betting","what":{"sport":["id","name","alias","order"],"competition":["id","order","name"],"region":["id","name","alias"],"game":[["id","start_ts","team1_name","team2_name","type","info","stats","markets_count","is_blocked","video_provider"]]},"where":{"game":{"type":1},"sport":{"id":{"@in":[1]}}},"subscribe":false},"rid":"2"}')
             ws.send(
                 '{"command": "get", "params": {"source": "betting", "is_betslip": true, "what": {"sport":["id"],"game": ["id", "is_blocked", "team1_name", "team2_name", "team1_reg_name", "team2_reg_name", "is_live"], "market": ["base", "type", "name", "home_score", "away_score", "cashout", "extra_info"], "event": ["id", "price", "type", "type_1", "name", "base", "ew_allowed"]},"where":{"sport":{"id":{"@in":[1]}},"game":{"is_live":1}}}, "subscribe": false}, "rid": "2"}')
-            sleep(2)
+            sleep(15)
 
     def on_open(self, ws:websocket.WebSocketApp):
-        logg.info(f"Websocket({ws}) connection opened.")
+        self.logg.info(f"Websocket({ws}) connection opened.")
+        self.pill2kill.clear()
         ws.send(
             f'{{"command":"request_session","params":{{"language":"eng","site_id":1,"release_date":"26/05/2022-12:10"}},"rid":{2}}}')
         """
@@ -74,20 +74,46 @@ class WebsocketClient:
                 markets_info=self.parsers.markets_parser(raw_data,old_markets)
                 self.markets_redis.save_markets(markets_info)
 
-    def on_error(self, ws: websocket.WebSocketApp, error):
-        """
-        Used as a trigger to call retrier func which in turn handles errors and exceptions.
-        """
-        raise error
+    
+    def on_error(self, ws: websocket.WebSocketApp, error:websocket.WebSocketException):
+        self.pill2kill.set()
+        if error.__str__() == "Connection to remote host was lost.":
+            if self.conn_closed_count < 3:
+                self.conn_closed_count+=1
+            else:
+                self.logg.warning(f"{error}. Number od disconnections {self.conn_closed_count}")
+                self.conn_closed_count = 0
+                self.pill2kill.clear()
+                ws.close()
+        else:
+            self.error_count += 1
+            if self.error_count < 3:
+                self.logg.error(f"{error}. Number of errors {self.error_count}")
+                #sleep(3)
+            else:
+                
+                self.logg.error(f"{error}. Number of errors {self.error_count}", exc_info=True)
+                self.error_count=0
+                self.pill2kill.clear()
+                ws.close()
+    
+    def on_close(self,ws, close_status_code, close_msg):
+        if self.pill2kill.is_set():
+            self.logg.warning(f"CLOSED CONNECTION!")
+            sleep(1)
+            self.connection()
+        else:
+            return
 
-    @auto_recovery
+    # @auto_recovery
     def connection(self) -> None:
-        websocket.enableTrace(traceable=True, handler=fh)
+        websocket.enableTrace(traceable=False, handler=None)
         websocket.setdefaulttimeout(15)
         ws = websocket.WebSocketApp(self.ws_url,
                                     on_open=self.on_open,
                                     on_message=self.on_msg,
-                                    on_error=self.on_error)
+                                    on_error=self.on_error,
+                                    on_close=self.on_close)
         """
         adding skip_utf8_validation=True for performance boost of cca 5%.
         """
